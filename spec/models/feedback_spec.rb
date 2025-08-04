33ac07cadac413ad63e423f80d3db3093e02b3ea
Neville <neville.every@gmail.com>
2025-08-04 01:01:28 -0400

    Add broadcast for real time updates

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Feedback, type: :model do
     it { should validate_inclusion_of(:rating).in_range(1..5) }
     it { should validate_presence_of(:comment) }
   end
+
+  describe 'after_create_commit' do
+    let(:event) { Event.create!(name: "Test Event") }
+
+    it 'broadcasts to turbo stream after creation' do
+      expect {
+        Feedback.create!(event: event, rating: 5, comment: "Excellent!")
+      }.to have_broadcasted_to("feedbacks").from_channel("Turbo::StreamsChannel")
+    end
+  end
 end
